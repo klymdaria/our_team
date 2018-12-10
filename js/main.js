ad5737ced5d24d713110359492beb8c069da5824
klymdaria <klimenkodaria@ukr.net>
2018-12-10 15:01:11 +0200

    add slider

---
+++ js/main.js
@@ -0,0 +1,16 @@
+;(function($) {
+	"use strict";
+
+	const teamSlider = $('.ba-team-slider');
+
+	teamSlider.slick({
+        dots: true
+    });
+    
+    const arrow = $('.slick-arrow').text();
+   console.log(arrow);
+   
+    
+	
+
+})(jQuery);
